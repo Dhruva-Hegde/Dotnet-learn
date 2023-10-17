0dc6fdcdced4d4d04611af69a2bb44efa25c7a07
Dhruva-Hegde <dhruvahegde55@gmail.com>
2023-10-17 15:28:16 +0530

    code update

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,18 @@
-﻿class Program {
-    static void Main(string[] args) {
-        string?[] carNames = new string?[4];
+﻿using System;
 
-        for (int i = 0; i < carNames.Length; i++) {
-            Console.Write($"\tEnter Car Name {i + 1}: ");
-            carNames[i] = Console.ReadLine();
+class Class3 {
+    static void Main(string[] args) {
+        string?[] cars = new string?[4];
+
+        for (int i = 0; i < cars.Length; i++) {
+            Console.Write($"\tElement[{i}]: ");
+            cars[i] = Console.ReadLine();
         }
 
-        Console.WriteLine("List of Cars:");
-        foreach (string? carName in carNames) {
-            if (!string.IsNullOrWhiteSpace(carName)) {
-                Console.WriteLine(carName);
+        Console.WriteLine("List of cars:");
+        foreach (string? car in cars) {
+            if (!string.IsNullOrWhiteSpace(car)) {
+                Console.WriteLine(car);
             }
         }
 
